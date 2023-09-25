c98023a6bd415ef7574a06ece607ab29d209df5a
“[Ali56758]” <[aayanali507@gmail.com]>
2023-09-26 02:11:36 +0500

    reverse number file added

---
+++ reverse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        while(num>0){
+            int rem = num%10;
+            System.out.print(rem);
+            num = num/10;
+        }
+    }
+}
